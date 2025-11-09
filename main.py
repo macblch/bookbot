b0286946825ae3e07bc14fad871b65ffdf0b3015
macblch <apps+github@macblch.me>
2025-11-09 20:15:55 +0100

    count num of words

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def get_book_text(path) -> str:
+def get_book_text(path: str) -> str:
     with open(path) as f:
         file_conents = f.read()
         return file_conents
 
 
+def get_num_of_words(text: str) -> int:
+    words = text.split(sep=None)
+    return len(words)
+
+
 def main():
     file_contents = get_book_text("./books/frankenstein.txt")
-    print(file_contents)
+    num_words = get_num_of_words(file_contents)
+    print(f"Found {num_words} total words")
 
 
 if __name__ == "__main__":
